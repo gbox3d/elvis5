d47caa13029d133c9c8ba0a78693a16514cfe79d
도플광어 <gbox3d@gmail.com>
2016-03-24 09:41:39 +0900

    2d effect example begine

--- elvis3d/libs/threejs/three.js
+++ elvis3d/libs/threejs/three.js
@@ -23280,12 +23280,13 @@ THREE.Sprite = ( function () {
 
 	var indices = new Uint16Array( [ 0, 1, 2,  0, 2, 3 ] );
 	var vertices = new Float32Array( [ - 0.5, - 0.5, 0,   0.5, - 0.5, 0,   0.5, 0.5, 0,   - 0.5, 0.5, 0 ] );
-	var uvs = new Float32Array( [ 0, 0,   1, 0,   1, 1,   0, 1 ] );
+	//var uvs = new Float32Array( [ 0, 0,   1, 0,   1, 1,   0, 1 ] );
+	//var uvs = new Float32Array( [ ] );
 
 	var geometry = new THREE.BufferGeometry();
 	geometry.setIndex( new THREE.BufferAttribute( indices, 1 ) );
 	geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
-	geometry.addAttribute( 'uv', new THREE.BufferAttribute( uvs, 2 ) );
+	//geometry.addAttribute( 'uv', new THREE.BufferAttribute( uvs, 2 ) );
 
 	return function Sprite( material ) {
 
